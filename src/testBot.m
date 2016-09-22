8587a8ae45904bc10c3f17fc625cc9a7dab2835f
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-09-22 14:22:21 +0200

    Testing of read and write functions. Made script that sets servos to a starting position for robot.

---
+++ src/testBot.m
@@ -0,0 +1,3 @@
+addpath /home/andreas/Documents/humanoidrobotics/roboloid/lib/DynamixelSDK/c/include
+addpath /home/andreas/Documents/humanoidrobotics/roboloid/lib/DynamixelSDK/c/build/linux64
+addpath(genpath('/home/andreas/Documents/humanoidrobotics/roboloid/lib/DynamixelSDK/matlab'))
---
+++ src/writeRobotPos.m
@@ -0,0 +1,173 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (c) 2016, ROBOTIS CO., LTD.
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% * Redistributions of source code must retain the above copyright notice, this
+%   list of conditions and the following disclaimer.
+%
+% * Redistributions in binary form must reproduce the above copyright notice,
+%   this list of conditions and the following disclaimer in the documentation
+%   and/or other materials provided with the distribution.
+%
+% * Neither the name of ROBOTIS nor the names of its
+%   contributors may be used to endorse or promote products derived from
+%   this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+% DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+% OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Author: Ryu Woon Jung (Leon)
+
+%
+% *********     Read and Write Example      *********
+%
+%
+% Available DXL model on this example : All models using Protocol 1.0
+% This example is designed for using a Dynamixel MX-28, and an USB2DYNAMIXEL.
+% To use another Dynamixel model, such as X series, see their details in E-Manual(support.robotis.com) and edit below variables yourself.
+% Be sure that Dynamixel MX properties are already set as %% ID : 1 / Baudnum : 1 (Baudrate : 1000000 [1M])
+%
+
+clc;
+clear all;
+
+lib_name = '';
+
+if strcmp(computer, 'PCWIN')
+  lib_name = 'dxl_x86_c';
+elseif strcmp(computer, 'PCWIN64')
+  lib_name = 'dxl_x64_c';
+elseif strcmp(computer, 'GLNX86')
+  lib_name = 'libdxl_x86_c';
+elseif strcmp(computer, 'GLNXA64')
+  lib_name = 'libdxl_x64_c';
+end
+
+% Load Libraries
+if ~libisloaded(lib_name)
+    [notfound, warnings] = loadlibrary(lib_name, 'dynamixel_sdk.h', 'addheader', 'port_handler.h', 'addheader', 'packet_handler.h');
+end
+
+% Control table address
+ADDR_MX_TORQUE_ENABLE       = 24;           % Control table address is different in Dynamixel model
+ADDR_MX_GOAL_POSITION       = 30;
+ADDR_MX_PRESENT_POSITION    = 36;
+
+% Protocol version
+PROTOCOL_VERSION            = 1.0;          % See which protocol version is used in the Dynamixel
+
+% Default setting
+DXL_ID                      = 1;
+BAUDRATE                    = 1000000;
+DEVICENAME                  = '/dev/ttyUSB1';       % Check which port is being used on your controller
+                                            % ex) Windows: "COM1"   Linux: "/dev/ttyUSB0"
+
+TORQUE_ENABLE               = 1;            % Value for enabling the torque
+TORQUE_DISABLE              = 0;            % Value for disabling the torque
+DXL_MINIMUM_POSITION_VALUE  = 100;          % Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 4000;         % and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MOVING_STATUS_THRESHOLD = 10;           % Dynamixel moving status threshold
+
+ESC_CHARACTER               = 'e';          % Key for escaping loop
+
+COMM_SUCCESS                = 0;            % Communication Success result value
+COMM_TX_FAIL                = -1001;        % Communication Tx Failed
+
+% Initialize PortHandler Structs
+% Set the port path
+% Get methods and members of PortHandlerLinux or PortHandlerWindows
+port_num = portHandler(DEVICENAME);
+
+% Initialize PacketHandler Structs
+packetHandler();
+
+index = 1;
+dxl_comm_result = COMM_TX_FAIL;             % Communication result
+dxl_goal_position = 500;         % Goal position
+
+dxl_error = 0;                              % Dynamixel error
+dxl_present_position = 0;                   % Present position
+
+
+% Open port
+if (openPort(port_num))
+    fprintf('Succeeded to open the port!\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to open the port!\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+
+% Set port baudrate
+if (setBaudRate(port_num, BAUDRATE))
+    fprintf('Succeeded to change the baudrate!\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to change the baudrate!\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+startingPose = [188 831 243 765 513 0 347 651 500 524 516 507 523 501 511 513 498 522];
+% Enable Dynamixel Torque
+
+for i=2:length(startingPose)
+    DXL_ID = i;
+    write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE);
+    if getLastTxRxResult(port_num, PROTOCOL_VERSION) ~= COMM_SUCCESS
+        printTxRxResult(PROTOCOL_VERSION, getLastTxRxResult(port_num, PROTOCOL_VERSION));
+    elseif getLastRxPacketError(port_num, PROTOCOL_VERSION) ~= 0
+        printRxPacketError(PROTOCOL_VERSION, getLastRxPacketError(port_num, PROTOCOL_VERSION));
+    else
+        fprintf('Dynamixel has been successfully connected \n');
+    end
+    pause(0.1)
+end
+
+
+% Write goal position
+for i=2:length(startingPose)
+    DXL_ID = i;
+    write2ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_MX_GOAL_POSITION, startingPose(i));
+    if getLastTxRxResult(port_num, PROTOCOL_VERSION) ~= COMM_SUCCESS
+        printTxRxResult(PROTOCOL_VERSION, getLastTxRxResult(port_num, PROTOCOL_VERSION));
+    elseif getLastRxPacketError(port_num, PROTOCOL_VERSION) ~= 0
+        printRxPacketError(PROTOCOL_VERSION, getLastRxPacketError(port_num, PROTOCOL_VERSION));
+    end
+    pause(1)
+end
+
+
+% Disable Dynamixel Torque
+for i=2:length(startingPose)
+    DXL_ID = i;
+    write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE);
+    if getLastTxRxResult(port_num, PROTOCOL_VERSION) ~= COMM_SUCCESS
+        printTxRxResult(PROTOCOL_VERSION, getLastTxRxResult(port_num, PROTOCOL_VERSION));
+    elseif getLastRxPacketError(port_num, PROTOCOL_VERSION) ~= 0
+        printRxPacketError(PROTOCOL_VERSION, getLastRxPacketError(port_num, PROTOCOL_VERSION));
+    end
+    pause(0.1)
+end
+% Close port
+closePort(port_num);
+
+% Unload Library
+unloadlibrary(lib_name);
+
+close all;
+clear all;
