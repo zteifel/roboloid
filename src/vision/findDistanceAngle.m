4cac65df7904598fa37831b95380ef7c27cef764
ollenorelius <norelius.olle@gmail.com>
2016-09-28 13:56:25 +0200

    First upload of vision system

---
+++ src/vision/findDistanceAngle.m
@@ -0,0 +1,54 @@
+function [dist, angle, status, ball, robot] = findDistanceAngle(cam, camHeight, ballCol, robCol)
+ballSize = 200;
+robotSize = 1000;
+status = '';
+dist = -1;
+angle = 999;
+mapping = 1/(900*(camHeight/0.86));
+ball = [0,0];
+robot = [0,0];
+
+im = snapshot(cam);
+
+mask = maskImage(im, [205, 100, 0]);
+robotMask = maskImage(im, [200,255,50]);
+
+CC_ball = bwconncomp(mask);
+CC_robot = bwconncomp(robotMask);
+
+ballCentr = regionprops(CC_ball, 'centroid');
+ballAreaProps = regionprops(CC_ball, 'area');
+ballCoords = cat(1, ballCentr.Centroid);
+ballArea = cat(1, ballAreaProps.Area);
+
+robotCentr = regionprops(CC_robot, 'centroid');
+robotAreaProps = regionprops(CC_robot, 'area');
+robotCoords = cat(1, robotCentr.Centroid);
+robotArea = cat(1, robotAreaProps.Area);
+
+[~, I] = sort(abs(robotArea-robotSize));
+if size(I,1) > 0
+    robotX = robotCoords(I(1),1);
+    robotY = robotCoords(I(1),2);
+    robot = [robotX, robotY];
+else
+    status = strcat(status, 'Cant see robot ');
+end
+
+[~, I] = sort(abs(ballArea-ballSize));
+if size(I,1) > 0
+    ballX = ballCoords(I(1),1);
+    ballY = ballCoords(I(1),2);
+    ball = [ballX, ballY];
+else
+    status = strcat(status, 'Cant see ball ');
+end
+
+if strcmp(status, '')
+    v = [ballX - robotX, ballY-robotY];
+    dist = mapping*norm(v);
+    angle = atan2(v(1), v(2));
+end
+
+
+
